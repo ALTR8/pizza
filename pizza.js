d42ff271b93d5a74c44ae28424c68f6f0238a1ee
Alexandra Roth <alexandra.roth@vaynermedia.com>
2017-11-15 11:59:46 -0500

    need to make sort more efficient

--- pizza.js
+++ pizza.js
@@ -1,11 +1,11 @@
 const pizzas = require('./pizzas');
 
+let toppingsArray = [];
 let popularToppings = {};
 let topTwentyToppings = [];
+let toppingsList;
 let popularCombos = {};
 let topTwentyCombos = {};
-let toppingsArray = [];
-let toppingsList;
 
 for (let i = 0; i < pizzas.length; i++) {
     toppingsArray.push(pizzas[i].toppings);
@@ -27,7 +27,8 @@ for (let i = 0; i < pizzas.length; i++) {
     };
 };
 
-//sorts toppings by popularity
+//--------------------------SORTING - this needs to be better
+
 toppingsSorted = Object.keys(popularToppings).sort(function(a,b) {
     return popularToppings[b] - popularToppings[a]
 });
@@ -35,19 +36,14 @@ toppingsSorted = Object.keys(popularToppings).sort(function(a,b) {
 console.log("\x1b[40m", "\x1b[37m", 'Most popular pizza toppings - ');
 console.log("\x1b[40m", "\x1b[37m", " ");
 
-//isolate top twenty toppings and log
+
 for (let z = 0; z < 20; z++) {
     topTwentyToppings.push(toppingsSorted[z]);
     topTwentyToppings[z] === "alredo sauce" ? topTwentyToppings[z] = "alfredo sauce" : topTwentyToppings[z]
     console.log("\x1b[40m", "\x1b[37m", "🍕    " + (z+1)+". " + topTwentyToppings[z]);
 };
-
-//extra space for style :)
 console.log("\x1b[40m", "\x1b[37m", " ");
 
-//--------------------------COMBOS
-
-//sorts combos by popularity - keys and values are separate to sort
 combosSorted = Object.keys(popularCombos).sort(function(a,b) {
     return popularCombos[b] - popularCombos[a]
 });
@@ -62,7 +58,7 @@ comboValuesSorted = comboValuesIsolated.sort(function(a,b) {
 console.log("\x1b[40m", "\x1b[37m", "Most popular pizza combos - ");
 console.log("\x1b[40m", "\x1b[37m", " ");
 
-//isolate top twenty combos and log
+
 for (let z = 0; z < 20; z++) {
     let combo = combosSorted[z];
     let number = comboValuesSorted[z];
@@ -71,6 +67,4 @@ for (let z = 0; z < 20; z++) {
 
     console.log("\x1b[40m", "\x1b[37m", "🍕    " + (z+1) + ". " + combo +" was ordered "+ number+" times")
 };
-
-//extra space for style :)
 console.log(" ");
